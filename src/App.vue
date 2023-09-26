fbf3e105aaaf01eec056f720b4b1e858aacbf1df
YbPonio <ybponio@gmail.com>
2023-09-26 17:22:28 +0800

    project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <TodoHeader />
+    <RouterView />
+</template>
+
+<script setup>
+import TodoHeader from './components/TodoHeader.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Martian+Mono:wght@100;200;300;400;500;600;700;800&family=Roboto:wght@100;300;400;500;700;900&display=swap');
+* {
+  font-family: 'Martian Mono', monospace;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,51 @@
+<template>
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <h1>Vue Todos</h1>
+            </div>
+
+            <ul class="nav-routes">
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<style lang="scss" scoped>
+header {
+    background: #355070;
+
+    nav {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    }
+
+    .branding {
+        h1 {
+            font-size: 3em;
+        }
+    }
+
+    .nav-routes {
+        display: flex;
+        flex: 1;
+        justify-content: flex-end;
+        gap: 3em;
+        list-style: none;
+
+
+        a {
+            text-decoration: none;
+            color: inherit;
+            font-size: 1.5em;
+        }
+    }
+}
+</style>
